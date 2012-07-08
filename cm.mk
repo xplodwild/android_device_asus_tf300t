ea9cf8ac51b89ae68cf0ffc8f24d8d44b6b6d64d
Logan McNaughton <logan@bacoosta.com>
2012-07-08 01:16:31 -0600

    Update build prop: Use proper device name

--- cm.mk
+++ cm.mk
@@ -10,9 +10,9 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 PRODUCT_NAME := cm_tf300t
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := tf300t
-PRODUCT_MODEL := Transformer Prime TF300T
+PRODUCT_MODEL := Transformer Pad TF300T
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=EeePad BUILD_FINGERPRINT=asus/WW_epad/EeePad:4.0.3/IML74K/WW_epad-9.4.3.29-20120511:user/release-keys PRIVATE_BUILD_DESC="WW_epad-user 4.0.3 IML74K WW_epad-9.4.3.29-20120511 release-keys"
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := tf300t
+PRODUCT_RELEASE_NAME := TF300T
