9d7a83c7daf3cbabd901601bd85aa367704465e2
Logan McNaughton <logan@bacoosta.com>
2012-07-26 14:30:20 -0600

    Reverts

--- asusdec/java/com/cyanogenmod/asusdec/KeyHandler.java
+++ asusdec/java/com/cyanogenmod/asusdec/KeyHandler.java
@@ -40,10 +40,8 @@ import com.android.internal.os.DeviceKeyHandler;
 public final class KeyHandler implements DeviceKeyHandler {
     private static final String TAG = "AsusdecKeyHandler";
 
-//    private static final int MINIMUM_BACKLIGHT = android.os.PowerManager.BRIGHTNESS_OFF + 1;
-  //  private static final int MAXIMUM_BACKLIGHT = android.os.PowerManager.BRIGHTNESS_ON;
-    private static final int MINIMUM_BACKLIGHT = 1;
-    private static final int MAXIMUM_BACKLIGHT = 255;
+    private static final int MINIMUM_BACKLIGHT = android.os.PowerManager.BRIGHTNESS_OFF + 1;
+    private static final int MAXIMUM_BACKLIGHT = android.os.PowerManager.BRIGHTNESS_ON;
     private static final String SETTING_TOUCHPAD_STATUS = "touchpad_status";
 
     private final Context mContext;
